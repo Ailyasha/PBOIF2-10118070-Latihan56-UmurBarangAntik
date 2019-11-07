7e7c456c50940fdfc3a9b8e84003d71c269259a1
Aufa Ilyasha <55798997+Ailyasha@users.noreply.github.com>
2019-11-07 21:48:15 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118070/latihan56/umurbarangantik/BarangAntik.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118070.latihan56.umurbarangantik;
+
+/**
+ *
+ * @author Ailyasha
+ * NAMA     : Aufa Ilyasha
+ * Kelas    : IF2
+ * NIM      : 10118070
+ */
+class BarangAntik {
+    private final int umur;
+    
+    public BarangAntik(int umur) {
+        this.umur = umur;
+    }
+    
+    public void tampilUmur() {
+        System.out.println("Umur barang antik ini adalah : " + umur + " tahun.");
+    }
+    
+}
